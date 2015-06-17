218d5df82a1afd49fb6c437eae553aeec8eb9dab
Kyle Byrd <kbyrd@jibe.com>
2015-06-17 00:28:24 -0400

    added index; added evenemmiters

--- app/models/privatePost.js
+++ app/models/privatePost.js
@@ -1,18 +1,24 @@
 var mongoose = require('mongoose');
+var huddleSchema = require('../huddle.js').schema;
+var Huddle = require('../huddle.js').model;
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
-// Private This or That post schema
-module.exports = mongoose.model('privatePost',
-	{
-		title: String,      //limit this size
-		leftImage: String,  //url pointing to location of image
-		rightImage: String,  //url pointing to location of image
-		leftVotes: {type: Number, default: 0},
-		rightVotes: {type: Number, default: 0},
-		location: Array,  //[lat,lon]
-		userId: String,  //mongoose _id of a user
-		createdTime: {type: Date, default: new Date()},
-		categories: Array, //predetermined set of categories?
-		rank: Number,
-		huddles: {type: Array, default: []}    //[{huddleId: id, name: name, user: userId},...]
-	}
-);
+
+var PrivatePostSchema = new mongoose.Schema({
+	title: {type: String, required: true},  //TODO: limit this size
+	leftImage: String,  //url pointing to location of image
+	rightImage: String,  //url pointing to location of image
+	leftVotes: {type: Number, default: 0},
+	rightVotes: {type: Number, default: 0},
+	location: {type: [Number], required: true},  //[lat,lon]
+	userId: String,  //mongoose _id of a user
+	createdTime: {type: Date, default: new Date()},
+	categories: [String],
+	rank: Number,
+	huddles: [huddleSchema]
+});
+PrivatePostSchema.index({"huddles.huddleId": 1});
+
+
+module.exports = mongoose.model('privatePost', PrivatePostSchema);
